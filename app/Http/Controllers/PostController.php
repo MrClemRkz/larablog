fcf6f040836f925d538e66c8179a70b3eeb75ee5
MrClemRkz <awesomeisme007@gmail.com>
2017-05-30 22:45:15 +0530

    Deleting posts using post.destroy resource

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -115,6 +115,13 @@ class PostController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $post = Post::find($id);
+        
+        $post->delete();
+
+        Session::flash('success', 'Post successfully deleted');
+
+        return redirect()->route('posts.index');
+
     }
 }
