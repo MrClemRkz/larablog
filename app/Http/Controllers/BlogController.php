0113846a16cd04300213fcf0a0fd8b6f54f37027
MrClemRkz <awesomeisme007@gmail.com>
2017-09-18 07:31:50 +0530

    Completing the URL pattern with 'blog'

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -7,6 +7,13 @@ use App\Post;
 
 class BlogController extends Controller
 {
+
+    public function getIndex(){
+    	$posts = Post::paginate(10);
+
+    	return view('blog.index')->withPosts($posts);
+    }
+
     public function getSingle($slug){
     	// fetch on the datbase on slug
     	$post = Post::where('slug', '=', $slug)->first();
